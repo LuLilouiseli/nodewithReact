2bf7a3310e7784c3fab02ffa4bddc0d1a031afde
LuLilouiseli <49573781+LuLilouiseli@users.noreply.github.com>
2019-11-14 16:30:32 -0500

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+//dev.js -don't commit this
+module.exports = {
+    googleClientID: '776619334890-8sssipumkbopl2rcf4v453a865k74tad.apps.googleusercontent.com',
+    googleClientSecret: 'xQL8UPZ3NE1f4FZDRzufhFew',
+    mongoURI: 'mongodb+srv://louise:password1234@emaily-dev-yzztq.mongodb.net/anything?authSource=admin',
+    coookieKey:'fjfjfjfjfjfjfiejfejfejfiefia',
+    stripePublishableKey:'pk_test_SW2oalCyl1iRYAFs3I1McgNa00J7dkNZzC',
+    stripeSecretKey:'sk_test_sqgEx3ePil9YBGPvaH8mKFt900GzcGzwbe',
+    sendGridKey: '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',
+    redirectDomain:"http://localhost:3000"
+}
---
+++ config/prod.js
@@ -0,0 +1,17 @@
+// production keys
+//dev.js -don't commit this
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    coookieKey:process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+
+
+}
+
+
+
